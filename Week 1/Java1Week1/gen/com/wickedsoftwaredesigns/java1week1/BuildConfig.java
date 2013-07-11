7dfc420462b99d6d2f621eb7b5328c9581788507
mikesmithjr <mikesmithjr@fullsail.edu>
2013-07-10 21:28:07 -0500

    Moving Docs around

---
+++ Week 1/Java1Week1/gen/com/wickedsoftwaredesigns/java1week1/BuildConfig.java
@@ -0,0 +1,9 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wickedsoftwaredesigns.java1week1;
+
+/**
+ * The Class BuildConfig.
+ */
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
