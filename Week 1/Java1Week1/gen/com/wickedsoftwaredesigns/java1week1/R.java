83cfbaac5e94103a5865fe4a56aa554b2d1102f3
mikesmithjr <mikesmithjr@fullsail.edu>
2013-07-10 23:30:13 -0500

    Working on adding Pilot Lists and Plane Lists

--- Week 1/Java1Week1/gen/com/wickedsoftwaredesigns/java1week1/R.java
+++ Week 1/Java1Week1/gen/com/wickedsoftwaredesigns/java1week1/R.java
@@ -27,10 +27,10 @@ public final class R {
         public static final int action_settings=0x7f080000;
     }
     public static final class integer {
-        public static final int dime=0x7f040001;
-        public static final int nickel=0x7f040002;
-        public static final int penny=0x7f040003;
-        public static final int quarter=0x7f040000;
+        public static final int plane1=0x7f040000;
+        public static final int plane2=0x7f040001;
+        public static final int plane3=0x7f040002;
+        public static final int plane4=0x7f040003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -38,12 +38,16 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int dime=0x7f050006;
         public static final int hello_world=0x7f050003;
         public static final int moneyamts=0x7f050004;
-        public static final int nickel=0x7f050007;
-        public static final int penny=0x7f050008;
-        public static final int quarter=0x7f050005;
+        public static final int pilot1=0x7f050005;
+        public static final int pilot2=0x7f050006;
+        public static final int pilot3=0x7f050007;
+        public static final int pilot4=0x7f050008;
+        public static final int plane1=0x7f050009;
+        public static final int plane2=0x7f05000a;
+        public static final int plane3=0x7f05000b;
+        public static final int plane4=0x7f05000c;
         public static final int title_activity_main=0x7f050001;
     }
     public static final class style {
