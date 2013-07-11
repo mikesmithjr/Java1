6199c34c7c090569da9ea36e73909096504adc80
Mike Smith <mikesmithjr@fullsail.edu>
2013-07-11 16:19:40 -0500

    setting up on work pc

--- Week 1/Java1Week1/gen/com/wickedsoftwaredesigns/java1week1/BuildConfig.java
+++ Week 1/Java1Week1/gen/com/wickedsoftwaredesigns/java1week1/BuildConfig.java
@@ -1,9 +1,6 @@
 /** Automatically generated file. DO NOT MODIFY */
 package com.wickedsoftwaredesigns.java1week1;
 
-/**
- * The Class BuildConfig.
- */
 public final class BuildConfig {
     public final static boolean DEBUG = true;
 }
